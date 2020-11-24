fb700332a937543eb5a695f275fbcaca9d745e7a
‪Taha Al-rehany‬‏ <32350359+taharehany@users.noreply.github.com>
2020-11-25 00:41:23 +0200

    Create script.js

---
+++ Portland/js/script.js
@@ -0,0 +1,137 @@
+/*global $*/
+$(function () {
+    "use strict";
+
+    $('document').ready(function () {
+        //****************//
+        $("#owl-demo").owlCarousel({
+            loop: true,
+            margin: 0,
+            nav: true,
+            items: 1,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 1
+                },
+                1000: {
+                    items: 1
+                }
+            }
+
+        });
+
+
+
+
+        var bigimage = $(".best-seller-content");
+        var thumbs = $("#thumbs");
+        //var totalslides = 10;
+        var syncedSecondary = true;
+
+        bigimage
+            .owlCarousel({
+                items: 4,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    600: {
+                        items: 2
+                    },
+                    1000: {
+                        items: 4
+                    }
+                },
+                slideSpeed: 2000,
+                nav: false,
+                autoplay: true,
+                dots: false,
+                loop: true,
+                responsiveRefreshRate: 200,
+                navText: [
+                '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
+                '<i class="fa fa-arrow-right" aria-hidden="true"></i>'
+              ]
+            })
+            .on("changed.owl.carousel", syncPosition);
+
+        thumbs
+            .on("initialized.owl.carousel", function () {
+                thumbs.find(".owl-item").eq(0).addClass("current");
+            })
+            .owlCarousel({
+                items: 12,
+                responsive: {
+                    0: {
+                        items: 5
+                    },
+                    600: {
+                        items: 12
+                    },
+                    1000: {
+                        items: 14
+                    }
+                },
+                dots: false,
+                nav: false,
+                loop: false,
+                navText: [
+                '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
+                '<i class="fa fa-arrow-right" aria-hidden="true"></i>'
+              ],
+                smartSpeed: 200,
+                slideSpeed: 500,
+                responsiveRefreshRate: 100
+            })
+            .on("changed.owl.carousel", syncPosition2);
+
+        function syncPosition(el) {
+            //if loop is set to false, then you have to uncomment the next line
+            //var current = el.item.index;
+
+            //to disable loop, comment this block
+            var count = el.item.count - 1;
+            var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
+
+            if (current < 0) {
+                current = count;
+            }
+            if (current > count) {
+                current = 0;
+            }
+            //to this
+            thumbs
+                .find(".owl-item")
+                .removeClass("current")
+                .eq(current)
+                .addClass("current");
+            var onscreen = thumbs.find(".owl-item.active").length - 1;
+            var start = thumbs.find(".owl-item.active").first().index();
+            var end = thumbs.find(".owl-item.active").last().index();
+
+            if (current > end) {
+                thumbs.data("owl.carousel").to(current, 100, true);
+            }
+            if (current < start) {
+                thumbs.data("owl.carousel").to(current - onscreen, 100, true);
+            }
+        }
+
+        function syncPosition2(el) {
+            if (syncedSecondary) {
+                var number = el.item.index;
+                bigimage.data("owl.carousel").to(number, 100, true);
+            }
+        }
+
+        thumbs.on("click", ".owl-item", function (e) {
+            e.preventDefault();
+            var number = $(this).index();
+            bigimage.data("owl.carousel").to(number, 300, true);
+        });
+
+    });
+});
